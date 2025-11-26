053c27b2cf3a46c11b000e4dc65a9fde6fe308d6
amiramostafaemam <amiraaemaam62@gmail.com>
2025-11-26 17:50:12 +0200

    Explore button edition

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const ExploreBtn = () => {
   return (
@@ -10,15 +11,15 @@ const ExploreBtn = () => {
       className="mt-7 mx-auto"
       onClick={() => console.log("clicked")}
     >
-      <a href="#events">
+      <Link href="/events">
         Explore Events
         <Image
-          src="/icons/arrow-down.svg"
-          alt="Arrow Down Icon"
+          src="/icons/arrow-diagonalup.svg"
+          alt="Arrow Up Icon"
           width={24}
           height={24}
         />
-      </a>
+      </Link>
     </button>
   );
 };
